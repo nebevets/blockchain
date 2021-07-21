6a24f7bc3895bb306b255c62196d47690a3d70ed
Steve Benedict <nebevets@Steves-MBP.attlocal.net>
2021-07-20 17:47:25 -0700

    create initial blockchain structure

---
+++ dev/blockchain.js
@@ -0,0 +1,58 @@
+const sha256 = require("sha256");
+
+class Blockchain {
+  constructor() {
+    this.chain = [];
+    this.pendingTransactions = [];
+    // create genesis block
+    this.createNewBlock(0, "0", "0");
+  }
+
+  createNewBlock(nonce, previousBlockHash, hash) {
+    const newBlock = {
+      hash,
+      index: this.chain.length + 1,
+      nonce,
+      previousBlockHash,
+      timestamp: Date.now(),
+      transactions: [...this.pendingTransactions],
+    };
+    this.chain.push(newBlock);
+    this.pendingTransactions = [];
+
+    return newBlock;
+  }
+
+  createNewTransaction(amount, sender, recipient) {
+    const newTransaction = {
+      amount,
+      recipient,
+      sender,
+    };
+    this.pendingTransactions.push(newTransaction);
+
+    return this.getLastBlock()["index"] + 1;
+  }
+
+  getLastBlock() {
+    return this.chain[this.chain.length - 1];
+  }
+
+  hashBlock(previousBlockHash, currentBlockData, nonce) {
+    const dataString =
+      previousBlockHash + nonce.toString() + JSON.stringify(currentBlockData);
+    return sha256(dataString);
+  }
+
+  proofOfWork(previousBlockHash, currentBlockData) {
+    let nonce = 0,
+      hash;
+    do {
+      hash = this.hashBlock(previousBlockHash, currentBlockData, nonce++);
+    } while (hash.substring(0, 4) !== "0000");
+
+    return nonce - 1;
+  }
+}
+
+module.exports = Blockchain;
