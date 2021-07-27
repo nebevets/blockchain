55cd5c2d84c6ac614f72c9fcbed50adf65f33b72
nebevets <steveben@exmsft.com>
2021-07-27 11:16:19 -0700

    created broadcast transaction

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,3 +1,4 @@
+const { getFormattedUUID } = require("./helpers");
 const sha256 = require("sha256");
 const DOMAIN = process.argv[3];
 const PORT = process.argv[2];
@@ -28,13 +29,16 @@ class Blockchain {
   }
 
   createTransaction(amount, sender, recipient) {
-    const newTransaction = {
+    return {
       amount,
       recipient,
       sender,
+      transactionID: getFormattedUUID(),
     };
-    this.pendingTransactions.push(newTransaction);
+  }
 
+  addPendingTransaction(transaction) {
+    this.pendingTransactions.push(transaction);
     return this.getLastBlock()["index"] + 1;
   }
 
--- dev/network-node.js
+++ dev/network-node.js
@@ -1,11 +1,12 @@
+const { getFormattedUUID } = require("./helpers");
 const axios = require("axios");
 const Blockchain = require("./blockchain");
 const express = require("express");
-const { v4: uuidv4 } = require("uuid");
+
+const nodeAddress = getFormattedUUID();
 
 const app = express();
 const nebCoin = new Blockchain();
-const nodeAddress = uuidv4().split("-").join("");
 
 const PORT = process.argv[2];
 
@@ -87,9 +88,43 @@ app.post("/update-nodes", ({ body: { allNetworkNodes } }, res) => {
   res.send({ message: "nodes updated", success: true });
 });
 
-app.post("/transaction", ({ body: { amount, recipient, sender } }, res) => {
-  const blockIndex = nebCoin.createTransaction(amount, recipient, sender);
-  res.send({ note: `transaction to be added to block ${blockIndex}` });
-});
+app.post(
+  "/transaction",
+  ({ body: { amount, recipient, sender, transactionID } }, res) => {
+    const blockIndex = nebCoin.addPendingTransaction({
+      amount,
+      recipient,
+      sender,
+      transactionID,
+    });
+    res.send({
+      message: `transaction added to block: ${blockIndex}`,
+      success: true,
+    });
+  }
+);
+
+app.post(
+  "/transaction/broadcast",
+  ({ body: { amount, recipient, sender } }, res) => {
+    const newTransaction = nebCoin.createTransaction(amount, recipient, sender);
+    nebCoin.addPendingTransaction(newTransaction);
+    const promises = nebCoin.networkNodes.map((networkNode) => {
+      axios({
+        data: newTransaction,
+        method: "post",
+        url: `${networkNode}/transaction`,
+      });
+    });
+    Promise.all(promises)
+      .then(() => {
+        res.send({
+          message: "transaction broadcast complete",
+          success: true,
+        });
+      })
+      .catch((err) => console.log(err.message));
+  }
+);
 
 app.listen(PORT, () => console.log(`listening on port: ${PORT}`));
